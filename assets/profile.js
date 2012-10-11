652d600cbdfc3765f2240b0aa518bf20ee69113e
Rex Feng <rexfeng@gmail.com>
2012-10-11 00:54:59 -0400

    working alpha

--- assets/profile.js
+++ assets/profile.js
@@ -6,16 +6,13 @@ js to-do list
 [x] drag and drop, sort function
 [x] detect list sort action finish
 
-[-] map out js var prompt string object
+[x] map out js var prompt string object
 
-[ ] parse CurrentConfig to be able to update Preview & Copytext
+[x] parse CurrentConfig to be able to update BashPreview & Copytext
+[x] ensure on change of sort, check state of everything - keep terminal display & output copy in sync
 
-[ ] strip out <li>, turn into array
-[ ] create function to turn array into <li></li> in html
-
-[ ] ensure on change of sort, check state of everything - keep terminal display & output copy in sync
-[ ] after drag event, set config settings available section to start state (as if no change)
 [ ] wrap terminal text if chars > 80
+[ ] after drag event, set config settings available section to start state (as if no change)
 
 [?] get current time
 [ ] onclick #copytext, select all text
@@ -34,27 +31,39 @@ $(document).ready(function() {
       connectWith: ".connectedSortable",
       update: function(event, ui) {
       	update_page();
-//event.preventDefault(); <-- is this line needed?
       }
     }).disableSelection();
   });
 
 
 
+// var prompt = $('#prompt').html();
+// var current_config = $('#current_config').html();
+// var prompt_strings = $('#prompt_strings').html();
+// var copytext = $('#copytext').html();
+
 // update page function() upon page sort
 	function update_page() {
 
-		var prompt = $('#prompt').html();
-		var current_config = $('#current_config').html();
-		var prompt_strings = $('#prompt_strings').html();
-		var copytext = $('#copytext').html();
-
 		// get current config
+			var inputs = [];
+			$('ul#current_config li').each(function(){
+				inputs.push($(this).attr('class'));
+			});
 
-		// update bash preview
-		// if bash preview text > 80 chars, then wrap
+		// update bash preview & copytext
+			var preview = "";
+			var copygoeshere = "PS1=";
+			
+			for (var i = 0; i < inputs.length; i++) {
+				preview += psobj[inputs[i]].example_copy;
+				copygoeshere += psobj[inputs[i]].bash_string;
+			}
 
-		// update bash_profile copytext
+			$('#prompt').html(preview);
+			$('#copytext').html(copygoeshere);
+
+			// if bash preview text > 80 chars, then wrap
 
 		// reset current config (prompt strings)
 	}
@@ -68,256 +77,242 @@ $(document).ready(function() {
 					example_copy: how this may look in bash preview
 			long_description: a description (such as from 'man bash')
 	*/
-	var psobj = {							// prompt string objects
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+	var psobj = {					 // prompt string objects
+		// "": {
+		// 			         text_id: "psa", 
+		// 			     bash_string: "", 
+		// 			    example_copy: "", 
+		// 			long_description: ""
+		// 		},
+		"psd": {
+					         text_id: "psd", 
+					     bash_string: "\\d", 
+					    example_copy: "Wed Oct 31", 
+					long_description: "the date in Weekday Month Date format"
+		},
+		// "": {
+		// 			         text_id: "psdate", 
+		// 			     bash_string: "", 
+		// 			    example_copy: "", 
+		// 			long_description: ""
+		// },
+		// "": {
+		// 			         text_id: "pse", 
+		// 			     bash_string: "", 
+		// 			    example_copy: "", 
+		// 			long_description: ""
+		// },
+		"psh": {
+					         text_id: "psh", 
+					     bash_string: "\\h", 
+					    example_copy: "macbook", 
+					long_description: "the hostname up to the first period"
+		},
+		"pshost": {
+					         text_id: "pshost", 
+					     bash_string: "\\H", 
+					    example_copy: "macbook.air", 
+					long_description: "the hostname"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psj": {
+					         text_id: "psj", 
+					     bash_string: "\\j", 
+					    example_copy: "0", 
+					long_description: "the number of jobs currently managed by the shell"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psl": {
+					         text_id: "psl", 
+					     bash_string: "\\l", 
+					    example_copy: "ttys002", 
+					long_description: "the basename of the shell’s terminal device name"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psn": {
+					         text_id: "psn", 
+					     bash_string: "\\n", 
+					    example_copy: "<br>", 
+					long_description: "newline"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		// "": {
+		// 			         text_id: "psr", 
+		// 			     bash_string: "\\r", 
+		// 			    example_copy: "", 
+		// 			long_description: "carriage return"
+		// },
+		"pss": {
+					         text_id: "pss", 
+					     bash_string: "\\s", 
+					    example_copy: "-bash", 
+					long_description: "the name of the shell"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pstime24hms": {
+					         text_id: "pstime24hms", 
+					     bash_string: "\\t", 
+					    example_copy: "17:13:30", 
+					long_description: "the current time in 24-hour HH:MM:SS format"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pstime12hms": {
+					         text_id: "pstime12hms", 
+					     bash_string: "\\T", 
+					    example_copy: "05:13:30", 
+					long_description: "the current time in 12-hour HH:MM:SS format"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pstime12ampm": {
+					         text_id: "pstime12ampm", 
+					     bash_string: "\\@", 
+					    example_copy: "05:13 PM", 
+					long_description: "the current time in 12-hour AM PM format"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pstime24hhmm": {
+					         text_id: "pstime24hhmm", 
+					     bash_string: "\\A", 
+					    example_copy: "17:13", 
+					long_description: "the current time in 24-hour HH:MM format"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psu": {
+					         text_id: "psu", 
+					     bash_string: "\\u", 
+					    example_copy: "jon", 
+					long_description: "the username of the current user"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psv": {
+					         text_id: "psv", 
+					     bash_string: "\\v", 
+					    example_copy: "3.2", 
+					long_description: "the version of bash"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psversion": {
+					         text_id: "psversion", 
+					     bash_string: "\\V", 
+					    example_copy: "3.2.48", 
+					long_description: "the release of bash, version + patch level"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psw": {
+					         text_id: "psw", 
+					     bash_string: "\\w", 
+					    example_copy: "~/Desktop", 
+					long_description: "the current working directory"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psworking": {
+					         text_id: "psworking", 
+					     bash_string: "\\W", 
+					    example_copy: "Desktop", 
+					long_description: "the basename of the current working directory"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psbang": {
+					         text_id: "psbang", 
+					     bash_string: "!", 
+					    example_copy: "!", 
+					long_description: "bang"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
+		"pshash": {
+					         text_id: "pshash", 
+					     bash_string: "#", 
+					    example_copy: "#", 
+					long_description: "hash"
+		},
+		"psdollar": {
+					         text_id: "psdollar", 
+					     bash_string: "$", 
+					    example_copy: "$", 
+					long_description: "dollar sign"
+		},
+		// "": {
+		// 			         text_id: "psoctal", 
+		// 			     bash_string: "", 
+		// 			    example_copy: "", 
+		// 			long_description: ""
+		// },
+		"psbackslash": {
+					         text_id: "psbackslash", 
+					     bash_string: "\\\\", 
+					    example_copy: "\\", 
 					long_description: ""
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psleftbrack": {
+					         text_id: "psleftbrack", 
+					     bash_string: "[", 
+					    example_copy: "[", 
+					long_description: "left bracket"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psrightbrack": {
+					         text_id: "psrightbrack", 
+					     bash_string: "]", 
+					    example_copy: "]", 
+					long_description: "right bracket"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pszero": {
+					         text_id: "pszero", 
+					     bash_string: "0", 
+					    example_copy: "0", 
+					long_description: "zero"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pscaret": {
+					         text_id: "pscaret", 
+					     bash_string: "^", 
+					    example_copy: "^", 
+					long_description: "caret"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pssplat": {
+					         text_id: "pssplat", 
+					     bash_string: "*", 
+					    example_copy: "*", 
+					long_description: "splat"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psdash": {
+					         text_id: "psdash", 
+					     bash_string: "-", 
+					    example_copy: "-", 
+					long_description: "dash"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psunder": {
+					         text_id: "psunder", 
+					     bash_string: "_", 
+					    example_copy: "_", 
+					long_description: "underscore"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psleftbrace": {
+					         text_id: "psleftbrace", 
+					     bash_string: "{", 
+					    example_copy: "{", 
+					long_description: "left brace"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psrightbrace": {
+					         text_id: "psrightbrace", 
+					     bash_string: "}", 
+					    example_copy: "}", 
+					long_description: "right brace"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pscolon": {
+					         text_id: "pscolon", 
+					     bash_string: ":", 
+					    example_copy: ":", 
+					long_description: "colon"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"pscomma": {
+					         text_id: "pscomma", 
+					     bash_string: ",", 
+					    example_copy: ",", 
+					long_description: "comma"
 				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psperiod": {
+					         text_id: "psperiod", 
+					     bash_string: ".", 
+					    example_copy: ".", 
+					long_description: "period"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psquestion": {
+					         text_id: "psquestion", 
+					     bash_string: "?", 
+					    example_copy: "?", 
+					long_description: "question mark"
 		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-				},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
-		},
-		"": {
-					         text_id: "", 
-					     bash_string: "", 
-					    example_copy: "", 
-					long_description: ""
+		"psspace": {
+					         text_id: "psspace", 
+					     bash_string: " ", 
+					    example_copy: " ", 
+					long_description: "space"
 		}
 	};
-
-
-
-// convert li_to_array
-	function li_to_array(li) {
-
-	}
-
-
-
-// convert array_to_li
-	function array_to_li(array) {
-
-	}
 
 
 
@@ -326,4 +321,4 @@ $(document).ready(function() {
 
 
 
-}); // end doc ready
+}); // end DOM ready

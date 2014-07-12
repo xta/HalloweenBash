493b9d0d6e6e234adc67f863afc3339dba7e7cb9
Rex Feng <rexfeng@gmail.com>
2014-07-12 13:02:35 -0400

    change dictionary to promptString. add accessor methods for the prompt string keys

---
+++ assets/js/spec/lib/promptString-spec.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var promptString = require('../../lib/promptString');
+
+describe("#text_id", function () {
+  it("returns text id", function () {
+    var period_text_id = promptString.text_id('psperiod');
+    expect( period_text_id ).toBe('psperiod');
+  });
+});
+
+describe("#bash_string", function () {
+  it("returns bash string", function () {
+    var period_bash_string = promptString.bash_string('psperiod');
+    expect( period_bash_string ).toBe('.');
+  });
+});
+
+describe("#example_copy", function () {
+  it("returns example copy", function () {
+    var period_example_copy = promptString.example_copy('psperiod');
+    expect( period_example_copy ).toBe('.');
+  });
+});
+
+describe("#long_description", function () {
+  it("returns description", function () {
+    var period_long_description = promptString.long_description('psperiod');
+    expect( period_long_description ).toBe('period');
+  });
+});
